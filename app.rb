f73175ae77eab3199274c8ec741e7dc1619d2078
RealLukeMartin <lukemartinwebdev@gmail.com>
2015-11-25 15:19:32 -0600

    deploy test

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ require "sinatra"
 $stdout.sync = true
 
 get "/" do
-  "Hello from Flynn on port #{ENV["PORT"]} from container #{Socket.gethostname}\n"
+  "This is port #{ENV["PORT"]} from container #{Socket.gethostname}\n"
 end
